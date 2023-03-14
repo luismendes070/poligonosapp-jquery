573e9d589ec5241dfd074734ebb2779a8916e80e
luismendes070 <machadoluismendes@gmail.com>
2023-03-14 10:43:20 -0300

    1 file modified

--- index.js
+++ index.js
@@ -18,11 +18,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var polygon2 = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
-]).addTo(map);
+// create a red polygon from an array of LatLng points
+var latlngs = [[37, -109.05],[41, -109.03],[41, -102.05],[37, -102.04]];
+
+var polygon = L.polygon(latlngs, {color: 'red'}).addTo(map);
 
 function onMapClick(e) {
   alert("You clicked the map at " + e.latlng);
@@ -31,4 +30,4 @@ function onMapClick(e) {
 map.on('click', onMapClick);
 
 // zoom the map to the polygon
-map.fitBounds(polygon2.getBounds());
+map.fitBounds(polygon.getBounds());
