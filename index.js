4c38e2c2e187621a1b8354dbb9538bd7b537dfea
luismendes070 <machadoluismendes@gmail.com>
2023-03-14 09:20:42 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// Import stylesheets
+import './style.css';
+
+import { * as L } from 'leaflet';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<div id="map"></div>`;
+
+// initialize the map on the "map" div with a given center and zoom
+var map = L.map('map', {
+  center: [51.505, -0.09],
+  zoom: 13
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var polygon2 = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(map);
+
+function onMapClick(e) {
+  alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
